c027088f1b79685ff06e28fb721f0fcf858bac7b
ksy0228 <k8829882932@gmail.com>
2020-04-16 16:53:16 +0900

    chronometer

---
+++ Chapter6_practice2/exercise6-2_chronometer/src/main/java/com/example/exercise6_2_chronometer/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.exercise6_2_chronometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+
+    Chronometer timer;
+    Button startBtn, stopBtn, resetBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timer = findViewById(R.id.timer);
+        startBtn = findViewById(R.id.startBtn);
+        stopBtn = findViewById(R.id.stopBtn);
+        resetBtn = findViewById(R.id.resetBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.start();
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.stop();
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
+    }
+}
