0250550a28e31369202d41cd68809b88e9e92316
ksy0228 <k8829882932@gmail.com>
2020-06-04 11:59:21 +0900

    ex02_listView

---
+++ Chapter11_practice/ex02_listview/src/main/java/com/example/ex02_listview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.ex02_listview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    ListView listView;
+
+    // 리스트뷰에 표시할 데이터
+    //String[] dataArray = getStringArrayForListView(100);
+    ArrayList<String> dataList = getStringArrayForListView(100);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
+
+        // ArrayAdapter 객체 생성 : 각 항목을 표시할 view와 data 설정
+        // ArrayAdpater(Context context, int resource, T[] objects)
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.row, R.id.textView2, dataList);
+
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        // 리스트뷰에 adapter 연결
+        listView.setAdapter(adapter);
+
+        // 리스트뷰의 트겅 항목을 클릭했을 때 발생하는 이벤트 처리
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 화면의 TextView에 선택된 항목의 데이터 출력하기
+                textView.setText(dataList.get(position));
+            }
+        });
+    }
+
+    // 리스트뷰에 표시할 리스트 데이터 생성 메소드
+    private ArrayList<String> getStringArrayForListView(int count) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= count; i++) {
+            list.add("리스트 데이터" + i);
+        }
+        return list;
+    }
+}
